5fd557677a89b5389e9a45fe4033b0130ae2013b
sakibashrak <officialsakibashrak@gmail.com>
2020-03-01 03:20:29 +0600

    Laravel Installed. Authentication Added. Table created for User info and User Profile info. Tables are linked. For each user post, another table is created with one to many relation. Page is secured. Login is requred to see user info. But User profile and post are public and does not require Authentication. Images are saved in storage/app/public/upload folder and linked to public folder to access the images. Image is also secured so that user can only opload image.

---
+++ resources/views/profiles/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+  <div class="col-3 p-5">
+   <!-- <img src="https://source.unsplash.com/200x200/?instagram" class="rounded-circle"/> -->
+   <img src="https://qph.fs.quoracdn.net/main-qimg-eada0af1ba55ed58034e4a828dda8403" style="height: 200px; width: 200px" class="rounded-circle"/>
+  </div>
+  <div class="col-9 pt-5">
+  <div class="d-flex justify-content-between align-items-baseline">
+  <h1>{{ $user-> username }}</h1>
+  <a href="/p/create">Add New Post</a>
+  </div>
+   <div class='d-flex'>
+   <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
+   <div class="pr-5"><strong>23k</strong> followers</div>
+   <div class="pr-5"><strong>121</strong> followings</div>
+
+   </div>
+   <div>
+   <div class="pt-2 font-weight-bold">{{ $user->profile->title }}</div>
+   <div>{{ $user->profile->description }}</div>
+   <div><a href="#">{{ $user->profile->url }}</a></div>
+   </div>
+  </div>
+  </div>
+   <div class="row">
+
+   @foreach($user->posts as $post)
+   <div class="col-4 pb-4">
+      <img src="/storage/{{ $post -> image }}" class="w-100" /> 
+   </div>
+   @endforeach
+   </div>
+
+</div>
+@endsection
