d01b569eec7004a420053d875e4578172d21df89
sakibashrak <officialsakibashrak@gmail.com>
2020-03-02 20:47:22 +0600

    For each post of the user New view is created which will show post picture, title and post description. User profile edit option is added. User can edit profile title, description and url and profile image. So for this patch method is applied. The patch request is secured so that only logged in user can edit his or her profile only. To hide the edit profile option for without logged in user Profile Policy is installed.

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -11,7 +11,8 @@
   <div class="d-flex justify-content-between align-items-baseline">
   <h1>{{ $user-> username }}</h1>
   <a href="/p/create">Add New Post</a>
-  </div>
+</div>
+<a href="/profile/{{ $user->id }}/edit">Edit Profile</a>
    <div class='d-flex'>
    <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
    <div class="pr-5"><strong>23k</strong> followers</div>
@@ -25,11 +26,13 @@
    </div>
   </div>
   </div>
-   <div class="row">
+   <div class="row pt-5">
 
    @foreach($user->posts as $post)
    <div class="col-4 pb-4">
-      <img src="/storage/{{ $post -> image }}" class="w-100" /> 
+   <a href="/p/{{ $post->id }}">
+         <img src="/storage/{{ $post -> image }}" class="w-100" /> 
+      </a>
    </div>
    @endforeach
    </div>
